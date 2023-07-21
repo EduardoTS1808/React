7062b4f20479d213c7a46ad868e0341a911aba41
Eduardo Tavares <eduardo91423344@gmail.com>
2023-07-21 12:38:10 -0300

    utilizando material ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import './App.css';
+//import './App.css';
 import Header from './componets/Header';
-import CalculadorIMC from './componets/IMC';
-import Form from './componets/Form';
+// import CalculadorIMC from './componets/IMC';
+// import Form from './componets/Form';
 import FormP from './componets/FormP/Form';
 import Login from './componets/Login/LoginControl'
+import { Heading1 } from './componets/Styles/styles'
+
+
+import React from 'react';
+import Stack from '@mui/material/Stack'
+import Botao from '@mui/material/Button'
+
+import {Box,Container, createTheme} from '@mui/system'
+
+
 
 
 function App() {
   return (
+    
 
-   <div className="App">
-     <Header title="Aula-1 React"/>
-    {/* <CalculadorIMC/> */}
-    {/* <Form/> */}
-    <FormP title="props"/>
-    <Login  name={'Eduardo'}/>
-   </div>
+  
+  // <React.Fragment>
+
+    <Stack spacing={2} direction="row">
+     
+      <Botao variant="contained">contained</Botao>
+    
+
+    </Stack>
+  // <React.Fragment/>
 
   )
 }
---
+++ src/componets/Styles/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const  Heading1 = styled.h1`
+    font-size: 1.7rem;
+    margin: 2rem;
+    border : .2rem solid #5c7d9;
+    box-shadowwn: .5rem 1rem 1rem #f9f9f9;
+    color: #59a;
+    span{
+        display: block;
+        font-sezi: 70%;
+        color: #283
+    }
+
+`
